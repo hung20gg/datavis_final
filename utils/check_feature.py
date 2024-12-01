59ab6393137901c98a289b5ceeb24f845dc9285d
hung20gg <quanghung20gg@gmail.com>
2024-12-01 16:12:00 +0700

    0.559 Using mean instead of 0

--- utils/check_feature.py
+++ utils/check_feature.py
@@ -135,13 +135,13 @@ def power_scaler_col(df, n_jobs = None):
     df_result['Mask_nunique'] = df_result['Unique'] > 100
     
     # Skewness > 10 -> Power transformation
-    df_result['Mask_skew'] = df_result['Skewness'].abs() > 10
+    df_result['Mask_skew'] = df_result['Skewness'].abs() > 8
     
     # Kurtosis > 15 -> Power transformation
-    df_result['Mask_kurt'] = (df_result['Kurtosis']-3).abs() > 15
+    df_result['Mask_kurt'] = (df_result['Kurtosis']-3).abs() > 50
     
     # Missing > 9/10 dataset -> Scale
-    df_result['Mask_missing'] = df_result['Missing'] < 0.9 * df.shape[0]
+    df_result['Mask_missing'] = df_result['Missing'] < 0.5 * df.shape[0]
     
     df_result['Mask'] = df_result['Mask_nunique'] & (df_result['Mask_skew'] | df_result['Mask_kurt']) & df_result['Mask_missing']
     
@@ -157,6 +157,39 @@ def power_scaler_col(df, n_jobs = None):
     return power_col, standard_col, min_max_col
 
     
+def exp_scaler_col(df, n_jobs = None):
+    # Return which columns for power transformation, which for standard scaler
+    
+    df_num = df.select_dtypes(include=[np.number])
+    df_num.fillna(0, inplace=True)
+    
+    df_result = analyze_df(df, n_jobs)
+    
+    # Nunique < 100 -> Categorical feature -> Scale
+    df_result['Mask_nunique'] = df_result['Unique'] > 100
+    
+    # Skewness > 10 -> Power transformation
+    df_result['Mask_skew'] = df_result['Skewness'].abs() > 5
+    
+    # # Kurtosis > 15 -> Power transformation
+    # df_result['Mask_kurt'] = (df_result['Kurtosis']-3).abs() > 50
+    
+    # Missing > 9/10 dataset -> Scale
+    df_result['Mask_missing'] = df_result['Missing'] < 0.5 * df.shape[0]
+    
+    df_result['Mask'] = df_result['Mask_nunique'] & df_result['Mask_skew'] & df_result['Mask_missing']
+    
+    df_result['Min_Max'] = ( df_result['Min'] == 0) & (df_result['Max'] < 50) & (df_result['Unique'] < 50)
+    
+    df_result
+    
+    power_col = df_result[df_result['Mask'] == 1].Feature.tolist()
+    standard_col = df_result[(df_result['Mask'] == 0) & (df_result['Min_Max'] == 0)].Feature.tolist()
+    min_max_col = df_result[(df_result['Mask'] == 0) & (df_result['Min_Max'] == 1)].Feature.tolist()
+    
+    
+    return power_col, standard_col, min_max_col
+
 
 if __name__ == '__main__':
     df = pd.read_parquet('../data/df_train.parquet')
